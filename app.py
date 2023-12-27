d6305452a026341e633c30cc5cad5df2d2649562
CodeBreaker <123104446+atharva0192@users.noreply.github.com>
2023-12-27 23:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import tensorflow as tf
+import base64
 from PIL import Image
 import numpy as np
 
@@ -9,11 +10,11 @@ model = tf.keras.models.load_model('my_model.h5')
 # Define a function for model inference
 def predict(image):
     # Open and preprocess the image
-    img = Image.open(image).convert('RGB')  # Ensure image is in RGB format
-    img = img.resize((120, 120))  # Resize image to match model input size
-    img_array = np.array(img)  # Convert image to NumPy array
+    img = Image.open(image).convert('RGB')  # Ensure the image is in RGB format
+    img = img.resize((256, 256))  # Resize the image to match the model input size
+    img_array = np.array(img)  # Convert the image to a NumPy array
     img_array = img_array / 255.0  # Normalize pixel values to the range [0, 1]
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array = np.expand_dims(img_array, axis=0)  # Add a batch dimension
 
     # Make predictions using the loaded model
     predictions = model.predict(img_array)
@@ -21,16 +22,38 @@ def predict(image):
     return predictions
 
 # Streamlit app code
-st.title("Deep Learning Model Deployment")
+st.set_page_config(
+    page_title="Malaria Detection App",
+    page_icon="🩸",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+# Sidebar
+st.sidebar.title("Malaria Detection")
+st.sidebar.write(
+    "This app uses a deep learning model to predict whether an uploaded blood smear image is normal or infected with malaria."
+)
+
+# Main content
+st.title("Malaria Detection App")
+
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    # Display the uploaded image
-    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-    
-    # Perform prediction
-    result = predict(uploaded_file)
+    # Display the uploaded image with border
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True, output_format="JPEG", width=300, border_radius=10)
 
-    # Display the prediction results
-    st.write("Prediction Results:")
-    st.write(result)
+    # Perform prediction
+    if st.button("Predict"):
+        result = predict(uploaded_file)
+
+        # Display the prediction results
+        st.write("Prediction Results:")
+        prediction_label = "Normal" if result >= 0.5 else "Infected"
+        st.success(f"The image is predicted as {prediction_label}")
+
+# Footer
+st.markdown("---")
+st.write("Developed by Atharva Chavan")
+st.write("Copyright © 2023. All rights reserved.")
